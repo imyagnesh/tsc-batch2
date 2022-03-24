8c71df421960e8b0a7cf9a558823e0f5b077bf79
Yagnesh Modh <hey@yagneshmodh.com>
2022-03-24 11:55:26 +0530

    basic class completed

---
+++ task2.js
@@ -0,0 +1,12 @@
+// Create Class of female Cricketers
+
+
+// firstName(property)
+// lastName(property)
+// role(property) // -> batsman, bowler, all rounder, wicket keeper 
+// order(property) -> Opener
+// fullName(method)
+// isBatsman(method) -> true / false
+// getter and setter for firstname and lastName
+
+// 11 instancesr
