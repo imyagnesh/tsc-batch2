af3b7f14b95bf26899f2825b82b900eab8ffe385
Yagnesh Modh <hey@yagneshmodh.com>
2022-04-06 13:04:36 +0530

    added javascript for page

---
+++ contact.js
@@ -0,0 +1,43 @@
+const contactForm = document.getElementById("contactForm")
+
+const validate = (elements) => {
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        if(!element.value) {
+            const error = document.createElement("p");
+            const label = element.parentElement.getElementsByTagName("label");
+            error.innerHTML = `${label[0].innerText} is Required`; 
+            error.style = "color:red; font-size: 12px; font-weight: 300";
+            element.parentNode.appendChild(error)
+        }
+    }
+}
+
+contactForm.onsubmit = (event) => {
+    event.preventDefault();
+    console.log("form submitted");
+
+    const name = document.getElementById("txtName");
+    const email = document.getElementById("txtEmail");
+    const tel = document.getElementById("txtTel");
+    const subject = document.getElementById("txtSubject");
+    const message = document.getElementById("txtMessage");
+
+    validate([
+        name,
+        email,
+        tel,
+        subject,
+        message
+    ]);
+    
+    console.log({
+        name: name.value,
+        email: email.value,
+        tel: tel.value,
+        subject: subject.value,
+        message: message.value
+    });
+}
+
+console.log(contactForm);
