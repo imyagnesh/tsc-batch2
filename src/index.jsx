8a56c545c26de461289d13660e7f98c6d14f8caf
Yagnesh Modh <hey@yagneshmodh.com>
2022-04-12 12:27:02 +0530

    learn about css props and state

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,55 @@
-import React from 'react';
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
 import { createRoot } from 'react-dom/client';
+import './style.css';
 
 // 1. Component Name should start with capital letter
 // 2. return only single element from component
-// 3. inlinse will be as object
+// 3. inline css will be as object
 const backgroundColor = 'green';
 const color = 'white';
+// Props are immutable
 
-function App() {
-  return (
-    <>
-      <h1
-        style={{
-          backgroundColor,
-          color,
-        }}
-      >
-        Hello from component
-      </h1>
-      <input />
-    </>
-  );
+// Function Component
+// function App({ name, caption }) {
+//   return (
+//     <div className="container">
+//       <h1
+//         style={{
+//           backgroundColor,
+//           color,
+//         }}
+//       >
+//         {name}
+//       </h1>
+//       <h2>{caption}</h2>
+//       <input type="checkbox" />
+//     </div>
+//   );
+// }
+
+// React JS Component Will re-render only when props and state value change
+
+class App extends Component {
+  render() {
+    const { name, caption } = this.props;
+
+    return (
+      <div className="container">
+        <h1
+          style={{
+            backgroundColor,
+            color,
+          }}
+        >
+          {`Hello, ${name}`}
+        </h1>
+        <h2>{caption}</h2>
+        <input type="checkbox" />
+      </div>
+    );
+  }
 }
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<App name="Yagnesh Sharma" caption="react trainer" />);
