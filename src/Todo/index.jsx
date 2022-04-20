8d491e7ff0fe7c6c423c0bc4c352cd0538a90323
Yagnesh Modh <hey@yagneshmodh.com>
2022-04-20 12:24:39 +0530

    basic todo app setup

---
+++ src/Todo/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Todo extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h1>Todo App</h1>
+      </div>
+    );
+  }
+}
+
+export default Todo;
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,6 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-// import reactDom from 'react-dom';
 import { createRoot } from 'react-dom/client';
-import App from './app';
-import './style.css';
-
-// App.getDerivedStateFromProps = (props, state) => {
-//   console.log('getDerivedStateFromProps');
-//   return {
-//     greetUser: `Hello ${props.name}`,
-//   };
-// };
+import Todo from './Todo';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App name="Yagnesh Modh" caption="react trainer" />);
+root.render(<Todo />);
